151c7a1bd57ebd3ae2a27f60e97fcbb7ce79d354
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-28 21:53:55 -0400

    pushing of Fizz Buzz Challenge files to GitHub

---
+++ fizz.js
@@ -0,0 +1,19 @@
+var userClick = document.getElementById("user-click"),
+    clearOutput = document.getElementById("clear-output"),
+    displayArea = document.getElementById("display-area");
+
+    userClick.addEventListener("click", function() {
+        for(i = 1; i <= 100; i++) {
+            if(i % 3 === 0) {
+                displayArea.innerText += "Fizz! " + i + "\n";
+            } else if(i % 5 === 0) {
+                displayArea.innerText += "Buzz! " + i + "\n";
+            } else {
+                displayArea.innerText += i + "\n";
+            }
+        }         
+    });
+
+    clearOutput.addEventListener("click", function() {
+        displayArea.innerText = "";
+    });
